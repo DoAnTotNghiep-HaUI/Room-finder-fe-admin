222e77f662e9de63dd51fae0f2580fdc8ecd532d
ViDucThien <vithienym19@gmail.com>
2025-03-12 22:24:11 +0900

    initital

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import path from "path";
+
+export default defineConfig({
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true,
+    port: 3000,
+    open: false,
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+});
